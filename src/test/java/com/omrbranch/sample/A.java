a64524db3926ba89b6b14387518d1e3b4313bac9
Pavan123221 <pavanjypsy@gmail.com>
2025-10-24 20:31:20 +0530

    uodated

--- src/test/java/com/omrbranch/sample/A.java
+++ src/test/java/com/omrbranch/sample/A.java
@@ -38,7 +38,7 @@ public class A {
 
 		Employee employee = new Employee(1, 6, 16961, 2930, data, support);
 		object.writeValue(file, employee);
-		System.out.println("update done in github");
+		System.out.println("just checking");
 
 	}
 
